530390559d6e7b42da5be93635fc35f06ff3a7c6
vivan-bhalla <55252892+vivan-bhalla@users.noreply.github.com>
2022-05-30 09:53:11 -0500

    Create WebsocketClientEndpoint.java

---
+++ src/main/java/com/baraka/minmaxservice/WebsocketClientEndpoint.java
@@ -0,0 +1,92 @@
+package com.baraka.minmaxservice;
+
+import javax.websocket.*;
+import java.net.URI;
+import java.nio.ByteBuffer;
+
+/**
+ * A client endpoint class that will serve as a helper to perform basic functions with the websocket endpoint.
+ */
+@ClientEndpoint
+public class WebsocketClientEndpoint {
+
+    Session userSession = null;
+    private MessageHandler messageHandler;
+
+    public WebsocketClientEndpoint(URI endpointURI) {
+        try {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            container.connectToServer(this, endpointURI);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Callback hook for Connection open events.
+     *
+     * @param userSession the userSession which is opened.
+     */
+    @OnOpen
+    public void onOpen(Session userSession) {
+        System.out.println("Websocket opened");
+        this.userSession = userSession;
+    }
+
+    /**
+     * Callback hook for Connection close events.
+     *
+     * @param userSession the userSession which is getting closed.
+     * @param reason the reason for connection close
+     */
+    @OnClose
+    public void onClose(Session userSession, CloseReason reason) {
+        System.out.println("Websocket Closed");
+        System.out.println(reason);
+        this.userSession = null;
+    }
+
+    /**
+     * Callback hook for Message Events. This method will be invoked when a client sends a message.
+     *
+     * @param message The text message
+     */
+    @OnMessage
+    public void onMessage(String message) {
+        if (this.messageHandler != null) {
+            this.messageHandler.handleMessage(message);
+        }
+    }
+
+    @OnMessage
+    public void onMessage(ByteBuffer bytes) {
+        System.out.println("Handle byte buffer");
+    }
+
+    /**
+     * Register message handler
+     *
+     * @param msgHandler
+     */
+    public void addMessageHandler(MessageHandler msgHandler) {
+        this.messageHandler = msgHandler;
+    }
+
+    /**
+     * Send a message.
+     *
+     * @param message
+     */
+    public void sendMessage(String message) {
+        this.userSession.getAsyncRemote().sendText(message);
+    }
+
+/**
+ * Message handler.
+ *
+ */
+public static interface MessageHandler {
+
+    public void handleMessage(String message);
+    }
+}
